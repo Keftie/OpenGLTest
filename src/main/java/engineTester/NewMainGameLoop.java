186e7eac160c419bf5bd91428515363cd5151c85
Kevin vdBosch <kevinvdbosch_1@hotmail.com>
2018-07-10 20:30:18 +0200

    initial commit

---
+++ src/main/java/engineTester/NewMainGameLoop.java
@@ -0,0 +1,10 @@
+package engineTester;
+
+import renderEngine.NewDisplayManager;
+
+public class NewMainGameLoop {
+
+	public static void main(String[] args) {
+		new NewDisplayManager().run();
+	}
+}
