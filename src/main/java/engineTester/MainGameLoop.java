3fe15bf9fb355216454ae79ca457f5664e5c0112
Kevin vdBosch <kevinvdbosch_1@hotmail.com>
2018-07-10 20:46:54 +0200

    rearrange code

--- src/main/java/engineTester/MainGameLoop.java
+++ src/main/java/engineTester/MainGameLoop.java
@@ -1,22 +1,45 @@
 package engineTester;
 
-/*import org.lwjgl.opengl.Display;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+
+import org.lwjgl.Version;
+import org.lwjgl.opengl.GL;
 
 import renderEngine.DisplayManager;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
+		System.out.println("Hello LWJGL " + Version.getVersion() + "!");
 		DisplayManager.createDisplay();
 		
-		while(!Display.isCloseRequested()) {
+		// This line is critical for LWJGL's interoperation with GLFW's
+		// OpenGL context, or any context that is managed externally.
+		// LWJGL detects the context that is current in the current thread,
+		// creates the GLCapabilities instance and makes the OpenGL
+		// bindings available for use.
+		GL.createCapabilities();
+		
+		// Set the clear color
+		glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+		
+		// Run the rendering loop until the user has attempted to close
+		// the window or has pressed the ESCAPE key.
+		while(!glfwWindowShouldClose(DisplayManager.window)) {
 			// game logic
 			
 			// render
 			
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+			glfwSwapBuffers(DisplayManager.window); // swap the color buffers
 			DisplayManager.updateDisplay();
 		}
 		
 		DisplayManager.closeDisplay();
 	}
-}*/
+}
--- src/main/java/renderEngine/DisplayManager.java
+++ src/main/java/renderEngine/DisplayManager.java
@@ -1,40 +1,96 @@
 package renderEngine;
 
-/*import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.ContextAttribs;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.PixelFormat;
+import org.lwjgl.glfw.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 public class DisplayManager {
 	
 	private static final int WIDTH = 1280;
 	private static final int HEIGHT = 720;
-	private static final int FPS_CAP = 120;
+	
+	public static long window;
 
 	public static void createDisplay() {
-		ContextAttribs attribs = new ContextAttribs(3,2)
-				.withForwardCompatible(true)
-				.withProfileCore(true);
+		// Setup an error callback. The default implementation
+		// will print the error message in System.err
+		GLFWErrorCallback.createPrint(System.err).set();
 		
-		try {
-			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
-			Display.create(new PixelFormat(), attribs);
-			Display.setTitle("OpenGL Test");
-		} catch (LWJGLException e) {
-			e.printStackTrace();
+		// Initialize GLFW. Most GLFW functions will not work before doing this.
+		if(!glfwInit()) {
+			throw new IllegalStateException("Unable to initialize GLFW");
 		}
 		
-		GL11.glViewport(0, 0, WIDTH, HEIGHT);
+		// Configure GLFW
+		glfwDefaultWindowHints(); // optional, the current window hints are already the default
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+		
+		// Create the window
+		window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Test", NULL, NULL);
+		if(window == NULL) {
+			throw new RuntimeException("Failed to create GLFW window");
+		}
+		
+		// Setup a key callback. It will be called every time a key is pressed, repeated or released
+		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+			if(key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+				glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+			}
+		});
+		
+		// Get the thread stack and push a new frame
+		try(MemoryStack stack = stackPush()) {
+			IntBuffer pWidth = stack.mallocInt(1); // int*
+			IntBuffer pHeight = stack.mallocInt(1); // int*
+			
+			// Get the window size passed to glfwCreateWindow
+			glfwGetWindowSize(window, pWidth, pHeight);
+			
+			// Get the resolution of the primary monitor
+			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+			
+			// Center the window
+			glfwSetWindowPos(
+					window,
+					(vidmode.width() - pWidth.get(0)) / 2,
+					(vidmode.height() - pHeight.get(0)) / 2
+			);
+		} // the stack frame is popped automatically
+		
+		// Make the OpenGL context current
+		glfwMakeContextCurrent(window);
+		// Enable v-sync
+		glfwSwapInterval(1);
+		
+		// Make the window visible
+		glfwShowWindow(window);
 	}
 	
 	public static void updateDisplay() {
-		Display.sync(FPS_CAP);
-		Display.update();
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+		
+		glfwSwapBuffers(window); // swap the color buffers
+		
+		// Poll for window events. The key callback above will only be
+		// invoked during this call.
+		glfwPollEvents();
 	}
 	
 	public static void closeDisplay() {
-		Display.destroy();
+		// Free the window callbacks and destroy the window
+		glfwFreeCallbacks(window);
+		glfwDestroyWindow(window);
+		
+		// Terminate GLFW and free the error callback
+		glfwTerminate();
+		glfwSetErrorCallback(null).free();
 	}
-}*/
+}
