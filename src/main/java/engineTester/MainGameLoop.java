64673737ca238664394c0806647a696615b0d5c3
Kevin vdBosch <kevinvdbosch_1@hotmail.com>
2018-07-11 19:34:30 +0200

    create a separate class to render the models

--- src/main/java/engineTester/MainGameLoop.java
+++ src/main/java/engineTester/MainGameLoop.java
@@ -25,17 +25,13 @@ public class MainGameLoop {
 		// bindings available for use.
 		GL.createCapabilities();
 		
-		// Set the clear color
-		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-		
 		// Run the rendering loop until the user has attempted to close
 		// the window or has pressed the ESCAPE key.
 		while(!glfwWindowShouldClose(DisplayManager.window)) {
 			// game logic
 			
 			// render
-			
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
 			glfwSwapBuffers(DisplayManager.window); // swap the color buffers
 			DisplayManager.updateDisplay();
 		}
